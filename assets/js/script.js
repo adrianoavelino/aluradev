149c3c48ba36790e0a7bba2939c160896d20d711
Adriano Avelino <adriano-st777@hotmail.com>
2021-05-26 22:56:57 -0300

    adiciona evento de clique para abrir e fechar navbar mobile

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+(function(){
+
+  const $button = document.querySelector('.hamburger');
+  const $navbar = document.querySelector('.navbar');
+  const $hamburger = document.querySelector('.hamburger img');
+  const srcIconNavbarClose = '/assets/img/navbar-close-icon.svg';
+  const srcIconNavbarOpen = '/assets/img/navbar-open-icon.svg';
+  
+  $button.addEventListener('click', function() {
+    console.log('click');
+    $navbar.classList.toggle('navbar--visible');
+    changeIconNavBarMobile();
+  });
+  
+  function changeIconNavBarMobile() {
+    $hamburger.src = (isOpenMobileNavbar())? srcIconNavbarClose : srcIconNavbarOpen;
+  }
+  
+  function isOpenMobileNavbar() {
+    return !!document.querySelector('img[src*="open"]');
+  }
+
+})();
+
