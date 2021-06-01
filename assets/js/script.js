919262f64023a9bceb26075c796d6d61e6276f6a
Adriano Avelino <adriano-st777@hotmail.com>
2021-05-31 21:10:35 -0300

    adiciona mudança de cor no background do editor

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,9 @@
   const $hamburger = document.querySelector('.hamburger img');
   const $language = document.querySelector('[data-js=project__language]');
   const $code = document.querySelector('[data-js=editor__code]');
+  const $EditorBackground = document.querySelector('[data-js=editor]');
   const $buttonPreview = document.querySelector('.button-preview');
+  const $color = document.querySelector('[data-js=project__color]');
   const srcIconNavbarClose = './assets/img/navbar-close-icon.svg';
   const srcIconNavbarOpen = './assets/img/navbar-open-icon.svg';
   let lastLanguage = $language.value;
@@ -38,6 +40,10 @@
 
   $buttonPreview.addEventListener('click', function() {
     hljs.highlightBlock($code);
+  });
+
+  $color.addEventListener('input', function() {
+    $EditorBackground.style.backgroundColor = $color.value;
   });
 
 })();
