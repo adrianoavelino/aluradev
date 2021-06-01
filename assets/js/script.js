bef707563402ea4a60583d0b9c330f188d46217b
Adriano Avelino <adriano-st777@hotmail.com>
2021-06-01 20:55:52 -0300

    separa script para exibição do menu mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,12 @@
 (function(){
 
-  const $button = document.querySelector('.hamburger');
-  const $navbar = document.querySelector('.navbar');
-  const $hamburger = document.querySelector('.hamburger img');
   const $language = document.querySelector('[data-js=project__language]');
   const $code = document.querySelector('[data-js=editor__code]');
   const $EditorBackground = document.querySelector('[data-js=editor]');
   const $buttonPreview = document.querySelector('.button-preview');
   const $color = document.querySelector('[data-js=project__color]');
-  const srcIconNavbarClose = './assets/img/navbar-close-icon.svg';
-  const srcIconNavbarOpen = './assets/img/navbar-open-icon.svg';
   let lastLanguage = $language.value;
 
-  $button.addEventListener('click', function() {
-    console.log('click');
-    $navbar.classList.toggle('navbar--visible');
-    changeIconNavBarMobile();
-  });
-  
-  function changeIconNavBarMobile() {
-    $hamburger.src = (isOpenMobileNavbar())? srcIconNavbarClose : srcIconNavbarOpen;
-  }
-  
-  function isOpenMobileNavbar() {
-    return !!document.querySelector('img[src*="open"]');
-  }
 
   $language.addEventListener('change', function() {
     updateEditorLanguage();
